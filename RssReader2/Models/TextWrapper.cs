a2e5e0401e4278c36e532585802f32cb1a8c4a76
Akn <sibata486@gmail.com>
2024-07-30 22:39:04 +0900

    Update version number.

--- RssReader2/Models/TextWrapper.cs
+++ RssReader2/Models/TextWrapper.cs
@@ -30,7 +30,7 @@ namespace RssReader2.Models
         [Conditional("RELEASE")]
         private void SetVersion()
         {
-            Version = "20240729" + "a";
+            Version = "20240730" + "a";
         }
 
         [Conditional("DEBUG")]
