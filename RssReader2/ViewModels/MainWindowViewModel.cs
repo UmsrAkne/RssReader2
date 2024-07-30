b402cb20ec64bd1863a697ac1fcc58b5bd0c729a
Akn <sibata486@gmail.com>
2024-07-30 22:11:59 +0900

    add / ウェブサイトを追加するページに使うファイルの追加・変更。

--- RssReader2/ViewModels/MainWindowViewModel.cs
+++ RssReader2/ViewModels/MainWindowViewModel.cs
@@ -1,22 +1,28 @@
 ﻿using System.Collections.ObjectModel;
+using Prism.Commands;
 using Prism.Ioc;
 using Prism.Mvvm;
+using Prism.Services.Dialogs;
 using RssReader2.Models;
 using RssReader2.Models.Dbs;
+using RssReader2.Views;
 
 namespace RssReader2.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class MainWindowViewModel : BindableBase
     {
+        private readonly IDialogService dialogService;
+
         public MainWindowViewModel()
         {
             Feeds = new ObservableCollection<Feed>(new DummyFeedProvider().GetAllFeeds());
             WebSiteTreeViewItems = new ObservableCollection<IWebSiteTreeViewItem>(new DummyWebSiteProvider().GetAllWebSites());
         }
 
-        public MainWindowViewModel(IContainerProvider containerProvider)
+        public MainWindowViewModel(IContainerProvider containerProvider, IDialogService dialogService)
         {
+            this.dialogService = dialogService;
             FeedProvider = containerProvider.Resolve<IFeedProvider>();
             FeedService = containerProvider.Resolve<FeedService>();
             Feeds = new ObservableCollection<Feed>(FeedProvider.GetAllFeeds());
@@ -32,6 +38,11 @@ namespace RssReader2.ViewModels
         public ObservableCollection<Feed> Feeds { get; set; }
 
         public ObservableCollection<IWebSiteTreeViewItem> WebSiteTreeViewItems { get; set; } = new ();
+
+        public DelegateCommand ShowWebSiteAdditionPageCommand => new DelegateCommand(() =>
+        {
+            dialogService.ShowDialog(nameof(WebSiteAdditionPage), new DialogParameters(), (_) => { });
+        });
 
         private IFeedProvider FeedProvider { get; set; }
     }
