4d6082373188620ab402c1e3f0e972a2429942b6
Akn <sibata486@gmail.com>
2024-07-31 15:39:58 +0900

    add / グループ追加画面に必要なファイルの追加・変更。

---
+++ RssReader2/ViewModels/GroupAdditionPageViewModel.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.ObjectModel;
+using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Services.Dialogs;
+using RssReader2.Models;
+using RssReader2.Models.Dbs;
+
+namespace RssReader2.ViewModels
+{
+    // ReSharper disable once ClassNeverInstantiated.Global
+    public class GroupAdditionPageViewModel : BindableBase, IDialogAware
+    {
+        private readonly WebSiteGroupService webSiteGroupService;
+        private ObservableCollection<WebSiteGroup> webSiteGroups;
+
+        public GroupAdditionPageViewModel(WebSiteGroupService webSiteGroupService)
+        {
+            this.webSiteGroupService = webSiteGroupService;
+            WebSiteGroups = new ObservableCollection<WebSiteGroup>(webSiteGroupService.GetAllWebSiteGroups());
+        }
+
+        public event Action<IDialogResult> RequestClose;
+
+        public string Title => string.Empty;
+
+        public string GroupName { get; set; }
+
+        public ObservableCollection<WebSiteGroup> WebSiteGroups
+        {
+            get => webSiteGroups;
+            set => SetProperty(ref webSiteGroups, value);
+        }
+
+        public DelegateCommand CloseCommand => new DelegateCommand(() =>
+        {
+            RequestClose?.Invoke(new DialogResult());
+        });
+
+        public bool CanCloseDialog() => true;
+
+        public void OnDialogClosed()
+        {
+        }
+
+        public void OnDialogOpened(IDialogParameters parameters)
+        {
+        }
+    }
+}
