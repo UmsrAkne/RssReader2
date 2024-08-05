b59cf14572e75cf0686277c0b5f59606cb53ac6f
Akn <sibata486@gmail.com>
2024-08-06 02:13:40 +0900

    add / FeedListViewModel を MainWindowViewModel に設置した。

--- RssReader2/ViewModels/FeedListViewModel.cs
+++ RssReader2/ViewModels/FeedListViewModel.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.ObjectModel;
+using Prism.Commands;
 using Prism.Mvvm;
 using RssReader2.Models;
 
@@ -7,17 +9,66 @@ namespace RssReader2.ViewModels
     // ReSharper disable once ClassNeverInstantiated.Global
     public class FeedListViewModel : BindableBase
     {
+        private bool hasNextPage;
+        private bool hasPrevPage;
+        private int totalPageNumber;
+        private int pageSize = 50;
+        private int pageNumber;
+        private ObservableCollection<Feed> feeds;
+        private WebSite webSite;
+
         public FeedListViewModel(IFeedProvider feedProvider)
         {
             FeedProvider = feedProvider;
         }
 
-        public ObservableCollection<Feed> Feeds { get; set; }
+        public ObservableCollection<Feed> Feeds { get => feeds; set => SetProperty(ref feeds, value); }
 
-        public int PageNumber { get; set; }
+        public int PageNumber { get => pageNumber; set => SetProperty(ref pageNumber, value); }
 
-        public int TotalPageNumber { get; set; }
+        public int PageSize { get => pageSize; set => SetProperty(ref pageSize, value); }
+
+        public int TotalPageNumber { get => totalPageNumber; set => SetProperty(ref totalPageNumber, value); }
+
+        public bool HasNextPage { get => hasNextPage; set => SetProperty(ref hasNextPage, value); }
+
+        public bool HasPrevPage { get => hasPrevPage; set => SetProperty(ref hasPrevPage, value); }
+
+        public WebSite WebSite
+        {
+            get => webSite;
+            set
+            {
+                if (SetProperty(ref webSite, value))
+                {
+                    ReloadFeeds();
+                }
+            }
+        }
+
+        public DelegateCommand ShowNextPageCommand => new DelegateCommand(() =>
+        {
+            Feeds = new ObservableCollection<Feed>(
+                FeedProvider.GetFeedsByWebSiteId(WebSite.Id, PageSize, ++PageNumber));
+        });
+
+        public DelegateCommand ShowPrevPageCommand => new DelegateCommand(() =>
+        {
+            Feeds = new ObservableCollection<Feed>(
+                FeedProvider.GetFeedsByWebSiteId(WebSite.Id, PageSize, --PageNumber));
+        });
 
         private IFeedProvider FeedProvider { get; set; }
+
+        public void ReloadFeeds()
+        {
+            Feeds = new ObservableCollection<Feed>(
+                FeedProvider.GetFeedsByWebSiteId(WebSite.Id, PageSize, PageNumber));
+
+            TotalPageNumber = (int)Math.Floor((double)FeedProvider.GetFeedCountByWebSiteId(WebSite.Id) / PageSize);
+            PageNumber = 0;
+            RaisePropertyChanged(nameof(HasNextPage));
+            RaisePropertyChanged(nameof(HasPrevPage));
+        }
     }
 }
