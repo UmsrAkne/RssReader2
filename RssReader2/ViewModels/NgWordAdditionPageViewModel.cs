2fcb0ab6c65d6ba9ebe756e0b5d0d5013ab50055
Akn <sibata486@gmail.com>
2024-08-09 18:23:52 +0900

    add feat / Ngワードを新規登録する処理を実装。

--- RssReader2/ViewModels/NgWordAdditionPageViewModel.cs
+++ RssReader2/ViewModels/NgWordAdditionPageViewModel.cs
@@ -1,16 +1,43 @@
 using System;
+using System.Collections.ObjectModel;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Services.Dialogs;
+using RssReader2.Models;
+using RssReader2.Models.Dbs;
 
 namespace RssReader2.ViewModels
 {
     // ReSharper disable once ClassNeverInstantiated.Global
     public class NgWordAdditionPageViewModel : BindableBase, IDialogAware
     {
+        private readonly NgWordService ngWordService;
+        private string ngWordText;
+
+        public NgWordAdditionPageViewModel(NgWordService ngWordService)
+        {
+            this.ngWordService = ngWordService;
+        }
+
         public event Action<IDialogResult> RequestClose;
 
         public string Title => string.Empty;
+
+        public string NgWordText { get => ngWordText; set => SetProperty(ref ngWordText, value); }
+
+        public ObservableCollection<NgWord> NgWords { get; set; }
+
+        public DelegateCommand AddNgWordCommand => new DelegateCommand(() =>
+        {
+            if (string.IsNullOrWhiteSpace(NgWordText))
+            {
+                return;
+            }
+
+            ngWordService.AddNgWord(new NgWord { Word = NgWordText, });
+            NgWords = new ObservableCollection<NgWord>(ngWordService.GetAllNgWords());
+            RaisePropertyChanged(nameof(NgWords));
+        });
 
         public DelegateCommand CloseCommand => new (() =>
         {
@@ -25,6 +52,8 @@ namespace RssReader2.ViewModels
 
         public void OnDialogOpened(IDialogParameters parameters)
         {
+            NgWords = new ObservableCollection<NgWord>(ngWordService.GetAllNgWords());
+            RaisePropertyChanged(nameof(NgWords));
         }
     }
 }
