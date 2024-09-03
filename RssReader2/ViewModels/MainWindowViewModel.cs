6fbc9daf16ac018e77c97c6420897c9c23a65671
Akn <sibata486@gmail.com>
2024-09-03 14:51:33 +0900

    add feat / 全サイトのフィードを自動更新する機能を実装し、それにアクセスするボタンを設置。

--- RssReader2/ViewModels/MainWindowViewModel.cs
+++ RssReader2/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Threading;
 using Prism.Commands;
 using Prism.Ioc;
 using Prism.Mvvm;
@@ -17,7 +18,9 @@ namespace RssReader2.ViewModels
     public class MainWindowViewModel : BindableBase
     {
         private readonly IDialogService dialogService;
+        private readonly DispatcherTimer timer;
         private bool uiEnabled = true;
+        private bool autoUpdate;
 
         [Obsolete("プレビュー用。ダミーを入力するためのコンストラクタです。明示的に呼び出さないでください。")]
         public MainWindowViewModel()
@@ -64,6 +67,17 @@ namespace RssReader2.ViewModels
             TreeViewVm.ReloadTreeViewItems();
 
             FeedListViewModel = containerProvider.Resolve<FeedListViewModel>();
+
+            timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromMinutes(90),
+                IsEnabled = AutoUpdate,
+            };
+
+            timer.Tick += (_, _) =>
+            {
+                _ = GetAllSiteRssFeedsCommandAsync.ExecuteAsync();
+            };
         }
 
         public TextWrapper TitleBarText { get; } = new ();
@@ -71,6 +85,25 @@ namespace RssReader2.ViewModels
         public TreeViewVm TreeViewVm { get; private init; }
 
         public bool UiEnabled { get => uiEnabled; set => SetProperty(ref uiEnabled, value); }
+
+        public bool AutoUpdate
+        {
+            get => autoUpdate;
+            set
+            {
+                switch (value)
+                {
+                    case false:
+                        timer.Stop();
+                        break;
+                    case true:
+                        timer.Start();
+                        break;
+                }
+
+                SetProperty(ref autoUpdate, value);
+            }
+        }
 
         public FeedListViewModel FeedListViewModel { get; init; }
 
