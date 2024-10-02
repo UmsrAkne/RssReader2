69829a750d9419af0b05b61ac2289d6621f1643a
Akn <sibata486@gmail.com>
2024-10-02 13:31:35 +0900

    refactoring / 余分なコードの削除。

--- RssReader2/Views/SettingPage.xaml.cs
+++ RssReader2/Views/SettingPage.xaml.cs
@@ -1,9 +1,8 @@
 using System.Windows;
-using System.Windows.Controls;
 
 namespace RssReader2.Views
 {
-    public partial class SettingPage : Page
+    public partial class SettingPage
     {
         public SettingPage()
         {
