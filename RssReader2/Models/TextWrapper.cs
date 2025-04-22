cf47573d016d941cf92bfbf3a7dc2fcf9d912723
UmsrAkne <sibata486@gmail.com>
2025-04-22 19:40:28 +0900

    update version number.

--- RssReader2/Models/TextWrapper.cs
+++ RssReader2/Models/TextWrapper.cs
@@ -30,7 +30,7 @@ namespace RssReader2.Models
         [Conditional("RELEASE")]
         private void SetVersion()
         {
-            Version = "20241121" + "a";
+            Version = "20250422" + "a";
         }
 
         [Conditional("DEBUG")]
